4c8e85341007d15ce09104ce62f9822061078075
Jonas Jonny <jonas.krutil@gmail.com>
2015-04-23 21:10:04 +0200

    Translucent effect
    To enable/disable translucent effect via attribute

--- iOS/ReactNativeIcons/IconTabBarItem/SMXTabBar.h
+++ iOS/ReactNativeIcons/IconTabBarItem/SMXTabBar.h
@@ -6,6 +6,7 @@
 
 @property (nonatomic, copy) UIColor *tintColor;
 @property (nonatomic, assign) UIColor *barTintColor;
+@property (nonatomic, assign) BOOL translucent;
 
 - (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher NS_DESIGNATED_INITIALIZER;
 
